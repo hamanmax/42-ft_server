12f6ea65090c1da8105fc6ae6fa9b1f73d4b6b59
Maximilien Haman <mhaman@student.le-101.fr>
2020-02-18 13:35:08 +0100

    nginx php wp ok ssl td

--- dockerfile
+++ dockerfile
@@ -3,5 +3,5 @@ RUN apt-get update -qq -y && apt-get upgrade -qq -y
 RUN apt-get install -qq -y nginx mariadb-server php7.3-common php7.3-cli php7.3-fpm php7.3-json php7.3-mysql php7.3-mbstring wget vim
 COPY srcs/default srcs/LEMP.sh srcs/www.conf srcs/config.inc.php srcs/wp-config.php ./
 RUN sh LEMP.sh
-#ENTRYPOINT service mysql start && service php7.3-fpm start && nginx -g'daemon off;'
+ENTRYPOINT service mysql start && service php7.3-fpm start && nginx -g'daemon off;'
 EXPOSE 80
--- srcs/LEMP.sh
+++ srcs/LEMP.sh
@@ -7,14 +7,15 @@ wget -q https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-l
 tar -zxf phpMyAdmin-4.9.0.1-all-languages.tar.gz && rm phpMyAdmin-4.9.0.1-all-languages.tar.gz
 mv phpMyAdmin-4.9.0.1-all-languages /usr/share/phpmyadmin && mv /config.inc.php /usr/share/phpmyadmin/config.sample.inc.php
 /etc/init.d/mysql start && \
-            mysql < /usr/share/phpmyadmin/sql/create_tables.sql -u root -p
+            mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE phpmyadmin" && echo "3" && \
+            mysql -u root -p${MYSQL_ROOT_PASSWORD} phpmyadmin < /usr/share/phpmyadmin/sql/create_tables.sql && \
             mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON *.* TO 'pma'@'localhost' IDENTIFIED BY 'pmapass'" && echo "1" && \
             mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES" && echo "2" && \
             mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE wpdatabase" && echo "3" && \
             mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE USER 'wpuser' IDENTIFIED BY 'wppass'"  && echo "4" && \
-            mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "GRANT USAGE ON wpdatabase.* TO 'wpuser'@localhost IDENTIFIED BY 'wppassword'"  && echo "5" && \
-            mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL privileges ON wpdatabase.* TO 'wpuser'@localhost" && echo "6"
-            mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES" && echo "7" && \
+            mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "GRANT USAGE ON wpdatabase.* TO 'wpuser'@localhost IDENTIFIED BY 'wppass'"  && echo "6" && \
+            mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL privileges ON wpdatabase.* TO 'wpuser'@localhost" && echo "7"
+            mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES" && echo "8" && \
 mkdir /usr/share/phpmyadmin/tmp
 chmod 777 /usr/share/phpmyadmin/tmp
 chown -R www-data:www-data /usr/share/phpmyadmin
